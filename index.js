b6a7feffb1966f30d64e2b4befb987f1a5d2286b
msrikanth508 <msrikanth508@gmail.com>
2017-03-19 20:34:05 +0530

    Add files via upload
    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var open = require('open');
+ 
+var app = express();
+ 
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+ 
+app.listen(process.env.PORT || 5000, function(err){
+    if (err) {
+    log(err);
+    return;
+  }
+  open(`http://localhost:5000/`);
+  console.log('🚧  App is listening at port 5000');
+});
+ 
+module.exports = app;
